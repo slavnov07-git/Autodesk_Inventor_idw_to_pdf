01430df15683b8090beede7cb5f26d25583d5414
slavnov07 <slavnov07@yandex.ru>
2022-01-23 21:50:48 +0300

    Добавьте файлы проекта.

---
+++ idw to pdf/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace idw_to_pdf
+{
+    partial class InventorIdwToPdf
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.InPath = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.OutPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 786);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 127);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InPath
+            // 
+            this.InPath.AccessibleName = "";
+            this.InPath.Location = new System.Drawing.Point(34, 35);
+            this.InPath.Name = "InPath";
+            this.InPath.Size = new System.Drawing.Size(1532, 23);
+            this.InPath.TabIndex = 1;
+            this.InPath.TextChanged += new System.EventHandler(this.InPath_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(34, 159);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1532, 514);
+            this.listBox1.TabIndex = 2;
+            // 
+            // OutPath
+            // 
+            this.OutPath.Location = new System.Drawing.Point(34, 82);
+            this.OutPath.Name = "OutPath";
+            this.OutPath.Size = new System.Drawing.Size(1532, 23);
+            this.OutPath.TabIndex = 3;
+            this.OutPath.TextChanged += new System.EventHandler(this.OutPath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Все найденные чертежи в указанной папке:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Путь к PDF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Путь к чертежам:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1572, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1572, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(379, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Требуемый формат: .idw";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(34, 680);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 19);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Ч/Б Печать";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(34, 706);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(135, 19);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Удалить веса линий";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "400";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "400",
+            "600",
+            "1200"});
+            this.comboBox1.Location = new System.Drawing.Point(495, 680);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(429, 684);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Качество:";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(34, 732);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(109, 19);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "Inventor_Visible";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(429, 732);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 65);
+            this.label6.TabIndex = 15;
+            // 
+            // InventorIdwToPdf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1635, 925);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutPath);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.InPath);
+            this.Controls.Add(this.button1);
+            this.Name = "InventorIdwToPdf";
+            this.Text = "InventorIdwToPdf";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InPath;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox OutPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ idw to pdf/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace idw_to_pdf
+{
+    public partial class InventorIdwToPdf : Form
+    {
+        public InventorIdwToPdf()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = "600";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            findIDWonDirectory(InPath.Text, out FileInfo[] Files);
+
+            //InvApp.Documents.Open(@"C:\Проекты\Inventor\SZ\Workspaces\Рабочее пространство\Бункер\СК004.00.000.idw");
+            //exportPDF(InvApp);
+            if (Files != null)
+            {
+                label6.Text = $"Выполнено: 0 из {Files.Length}";
+                OpenInventorFunc(out Inventor.Application InvApp);
+                int count = 1;
+                foreach (var file in Files)
+                {
+                    
+                    listBox1.Items.Add(file.FullName + "\r\n");
+                    InvApp.Documents.Open(file.FullName);
+                    exportPDF(InvApp);
+                    InvApp.ActiveDocument.Close(true);
+                    label6.Text = $"Выполнено: {count} из {Files.Length}";
+                    count++;
+                    
+                }
+                try
+                {
+                    foreach (Process proc in Process.GetProcessesByName("Inventor"))
+                    {
+                        proc.Kill();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("В указанной папке чертежей не обнаружено!", Text, MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+        }
+
+        private void OpenInventorFunc(out Inventor.Application InvApp)
+        {
+            string ProcName = "Inventor.Application";
+            try//получение обьекта приложения
+            {
+
+                InvApp = Marshal2.GetActiveObject(ProcName) as Inventor.Application;
+
+            }
+            catch
+            {
+                InvApp = Activator.CreateInstance(Type.GetTypeFromProgID(ProcName)) as Inventor.Application;
+                //InvApp.Visible = VisibleInventor.Checked;
+                InvApp.Visible = checkBox3.Checked;
+
+            }
+        }
+
+        private void findIDWonDirectory(string path, out FileInfo[] Files)
+        {
+
+            if (InPath.Text == "")
+            {
+                InPath.Text = @"C:\";
+            }
+
+            if (OutPath.Text == "")
+            {
+                OutPath.Text = @"C:\idw_to_pdf\";
+            }
+            if (!System.IO.Directory.Exists(InPath.Text))
+            {
+                MessageBox.Show("В указанной директории чертежей не обнаружено");
+            }
+
+            bool existsOutPath = System.IO.Directory.Exists(OutPath.Text);
+            if (!existsOutPath)
+            {
+                System.IO.Directory.CreateDirectory(OutPath.Text);
+            }
+
+            if (!System.IO.Directory.Exists(InPath.Text))
+            {
+                Files = null;
+                return;
+            }
+
+
+            DirectoryInfo d = new DirectoryInfo(InPath.Text);
+            Files = d.GetFiles("*.idw");
+
+        }
+
+        private void InPath_TextChanged(object sender, EventArgs e)
+        {
+            if (InPath.Text == "")
+            {
+                InPath.Text = @"C:\";
+            }
+        }
+
+        private void exportPDF(Inventor.Application InvApp)
+        {
+            // Get the PDF translator Add-In.
+            Inventor.TranslatorAddIn PDFAddin = InvApp.ApplicationAddIns.ItemById["{0AC6FD96-2F4D-42CE-8BE0-8AEA580399E4}"] as Inventor.TranslatorAddIn;
+
+            //Set a reference to the active document (the document to be published).
+
+            Inventor.Document oDocument = InvApp.ActiveDocument;
+
+            Inventor.TranslationContext oContext = InvApp.TransientObjects.CreateTranslationContext();
+            oContext.Type = Inventor.IOMechanismEnum.kFileBrowseIOMechanism;
+
+            // Create a NameValueMap object
+
+            Inventor.NameValueMap oOptions = InvApp.TransientObjects.CreateNameValueMap();
+
+            // Create a DataMedium object
+            Inventor.DataMedium oDataMedium = InvApp.TransientObjects.CreateDataMedium();
+
+            // Check whether the translator has 'SaveCopyAs' options
+            try
+            {
+                if (PDFAddin.HasSaveCopyAsOptions[oDocument, oContext, oOptions])
+                {
+                    // Options for drawings...
+                    if (checkBox1.Checked == true)
+                        oOptions.Value["All_Color_AS_Black"] = 1;
+                    else
+                        oOptions.Value["All_Color_AS_Black"] = 0;
+
+                    if(checkBox2.Checked == true)
+                        oOptions.Value["Remove_Line_Weights"] = 1;
+                    else
+                        oOptions.Value["Remove_Line_Weights"] = 0;
+
+                    if(comboBox1.Text == "600")
+                        oOptions.Value["Vector_Resolution"] = 600;
+                    if (comboBox1.Text == "400")
+                        oOptions.Value["Vector_Resolution"] = 400;
+                    if (comboBox1.Text == "1200")
+                        oOptions.Value["Vector_Resolution"] = 1200;
+
+                    oOptions.Value["Sheet_Range"] = Inventor.PrintRangeEnum.kPrintAllSheets;
+                    oOptions.Value["Custom_Begin_Sheet"] = 2;
+                    oOptions.Value["Custom_End_Sheet"] = 4;
+
+                }
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+
+            //string filePath = oDocument.FullFileName;
+            //filePath = filePath.Substring(0, filePath.Length - 4);
+            string NameDocument = oDocument.DisplayName;
+            NameDocument = NameDocument.Substring(0, NameDocument.Length - 4);
+            //Set the destination file name
+            //oDataMedium.FileName = filePath + ".pdf";
+
+            oDataMedium.FileName = OutPath.Text + "\\" + NameDocument + ".pdf";
+
+            //Publish document.
+            PDFAddin.SaveCopyAs(oDocument, oContext, oOptions, oDataMedium);
+
+            /*
+            <Project Sdk="Microsoft.NET.Sdk.WindowsDesktop">
+
+              <PropertyGroup>
+                <OutputType>WinExe</OutputType>
+                <TargetFramework>netcoreapp3.1</TargetFramework>
+                <RootNamespace>idw_to_pdf</RootNamespace>
+                <UseWindowsForms>true</UseWindowsForms>
+              </PropertyGroup>
+
+              <ItemGroup>
+                <Reference Include="Autodesk.Inventor.Interop">
+                  <HintPath>..\..\..\..\..\..\Windows\Microsoft.NET\assembly\GAC_MSIL\Autodesk.Inventor.Interop\v4.0_26.1.0.0__d84147f8b4276564\autodesk.inventor.interop.dll</HintPath>
+                </Reference>
+              </ItemGroup>
+
+            </Project> 
+            */
+
+        }
+
+        private void OutPath_TextChanged(object sender, EventArgs e)
+        {
+            if (OutPath.Text == "")
+            {
+                OutPath.Text = @"C:\idw_to_pdf\";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            selectFolder(InPath, e);
+            OutPath.Text = InPath.Text + @"\idw_to_pdf";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            selectFolder(OutPath, e);
+        }
+
+        void selectFolder (TextBox s, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult dialogResult = fbd.ShowDialog();
+            if (fbd.SelectedPath != null)
+            {
+                s.Text = fbd.SelectedPath;
+            }
+            else
+            {
+                s.Text = @"C:\";
+            }
+        }
+    }
+}
